97fc55471d43defe75a31765bdb90623f9c6a63f
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2022-11-27 05:50:19 -0300

    feat: adding IBM Plex Mono font family

--- src/configs/theme/fonts.ts
+++ src/configs/theme/fonts.ts
@@ -1,6 +1,7 @@
 export default {
   body: "'Inter', sans-serif",
   heading: "'Outfit', sans-serif",
+  mono: "'IBM Plex Mono', monospace",
   serif: "'Domine', serif",
   brand: "'Pacifico', serif"
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -31,6 +31,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100;200;300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
