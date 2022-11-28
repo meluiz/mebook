6cfe95c24d6050490d05f411e65ab519420e1a06
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2022-11-28 05:39:47 -0300

    feat: JetBrains Mono font family added

--- src/configs/theme/fonts.ts
+++ src/configs/theme/fonts.ts
@@ -2,6 +2,7 @@ export default {
   body: "'Inter', sans-serif",
   heading: "'Outfit', sans-serif",
   mono: "'IBM Plex Mono', monospace",
+  code: "'JetBrains Mono', monospace",
   serif: "'Domine', serif",
   brand: "'Pacifico', serif"
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,6 +35,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@100;200;300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;200;300;400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
